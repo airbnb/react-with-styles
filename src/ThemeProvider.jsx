9300301dacf7167db8fd82019623b83e292fb453
Joe Lencioni <joe.lencioni@airbnb.com>
2016-08-23 11:11:22 -0700

    Initial import of codes
    This commit mostly copies over code from where we originally wrote it. I
    made some small adjustments, like restructuring the directories and
    wiring up the import paths correctly, but for the most part this is a
    straight up copypasta.
    
    There's still plenty left to do, like set up testing, eslint, and babel,
    but I'll leave that for a later commit.

---
+++ src/ThemeProvider.jsx
@@ -0,0 +1,25 @@
+import React, { PropTypes } from 'react';
+
+const propTypes = {
+  children: PropTypes.node.isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+const childContextTypes = {
+  themeName: PropTypes.string,
+};
+
+export default class ThemeProvider extends React.Component {
+  getChildContext() {
+    return {
+      themeName: this.props.name,
+    };
+  }
+
+  render() {
+    return React.Children.only(this.props.children);
+  }
+}
+
+ThemeProvider.propTypes = propTypes;
+ThemeProvider.childContextTypes = childContextTypes;
---
+++ src/interfaces/aphroditeInterface.js
@@ -0,0 +1,74 @@
+import { StyleSheet, css } from 'aphrodite';
+
+import flatten from '../util/flatten';
+
+// This function takes the array of styles and separates them into styles that
+// are handled by Aphrodite and inline styles.
+function separateStyles(stylesArray) {
+  const aphroditeStyles = [];
+
+  // Since determining if an Object is empty requires collecting all of its
+  // keys, and we want the best performance in this code because we are in the
+  // render path, we are going to do a little bookkeeping ourselves.
+  let hasInlineStyles = false;
+  const inlineStyles = {};
+
+  // This is run on potentially every node in the tree when rendering, where
+  // performance is critical. Normally we would prefer using `forEach`, but
+  // old-fashioned for loops are faster so that's what we have chosen here.
+  for (let i = 0; i < stylesArray.length; i++) {
+    const style = stylesArray[i];
+
+    // If this  style is falsey, we just want to disregard it. This allows for
+    // syntax like:
+    //
+    //   css(isFoo && styles.foo)
+    if (style) {
+      if (
+        Object.prototype.hasOwnProperty.call(style, '_name') &&
+        Object.prototype.hasOwnProperty.call(style, '_definition')
+      ) {
+        // This looks like a reference to an Aphrodite style object, so that's
+        // where it goes.
+        aphroditeStyles.push(style);
+      } else {
+        Object.assign(inlineStyles, style);
+        hasInlineStyles = true;
+      }
+    }
+  }
+
+  return {
+    aphroditeStyles,
+    hasInlineStyles,
+    inlineStyles,
+  };
+}
+
+export default {
+  create(styleHash) {
+    return StyleSheet.create(styleHash);
+  },
+
+  // Styles is an array of properties returned by `create()`, a POJO, or an
+  // array thereof. POJOs are treated as inline styles.
+  // This function returns an object to be spread onto an element.
+  resolve(styles) {
+    const flattenedStyles = flatten(styles);
+
+    const {
+      aphroditeStyles,
+      hasInlineStyles,
+      inlineStyles,
+    } = separateStyles(flattenedStyles);
+
+    const result = {};
+    if (aphroditeStyles.length > 0) {
+      result.className = css(...aphroditeStyles);
+    }
+    if (hasInlineStyles) {
+      result.style = inlineStyles;
+    }
+    return result;
+  },
+};
