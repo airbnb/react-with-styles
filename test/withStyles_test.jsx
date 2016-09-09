151869235fa484117648686160aad577a79c5145
Joe Lencioni <joe.lencioni@airbnb.com>
2016-09-09 09:36:03 -0700

    Add test for withStyles
    We want this project to be robust against accidental breakages. Tests
    help us protect people from silly mistakes.
    
    Fixes #8.

---
+++ test/withStyles_test.jsx
@@ -0,0 +1,180 @@
+import React, { PropTypes } from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import sinon from 'sinon-sandbox';
+
+import ThemedStyleSheet from '../src/ThemedStyleSheet';
+import { css, withStyles } from '../src/withStyles';
+
+describe('withStyles()', () => {
+  const defaultTheme = {
+    color: {
+      red: '#990000',
+    },
+  };
+
+  let testInterface;
+
+  beforeEach(() => {
+    testInterface = {
+      create() {},
+      resolve() {},
+      flush: sinon.spy(),
+    };
+    sinon.stub(testInterface, 'create', styleHash => styleHash);
+    sinon.stub(testInterface, 'resolve', (styles) => ({
+      style: styles.reduce((result, style) => Object.assign(result, style)),
+    }));
+
+    ThemedStyleSheet.registerDefaultTheme(defaultTheme);
+    ThemedStyleSheet.registerInterface(testInterface);
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  describe('without a styleFn', () => {
+    it('returns the HOC function', () => {
+      expect(typeof withStyles()).to.equal('function');
+    });
+
+    it('does not create styles', () => {
+      withStyles();
+      expect(testInterface.create.callCount).to.equal(0);
+    });
+  });
+
+  it('returns the HOC function', () => {
+    expect(typeof withStyles(() => ({}))).to.equal('function');
+  });
+
+  it('creates the styles', () => {
+    withStyles(() => ({}));
+    expect(testInterface.create.callCount).to.equal(1);
+  });
+
+  describe('HOC', () => {
+    it('has a wrapped displayName', () => {
+      function MyComponent() {
+        return null;
+      }
+
+      const result = withStyles(() => ({}))(MyComponent);
+      expect(result.displayName).to.equal('withStyles(MyComponent)');
+    });
+
+    it('passes the theme to the wrapped component', () => {
+      function MyComponent({ theme }) {
+        expect(theme).to.eql(defaultTheme);
+        return null;
+      }
+
+      const Wrapped = withStyles(() => ({}))(MyComponent);
+      shallow(<Wrapped />);
+    });
+
+    it('allows the theme prop name to be customized', () => {
+      function MyComponent({ foo }) {
+        expect(foo).to.eql(defaultTheme);
+        return null;
+      }
+
+      const Wrapped = withStyles(() => ({}), { themePropName: 'foo' })(MyComponent);
+      shallow(<Wrapped />);
+    });
+
+    it('passes processed styles to wrapped component', () => {
+      function MyComponent({ styles }) {
+        expect(styles).to.eql({ foo: { color: '#ff0000' } });
+        return null;
+      }
+
+      const Wrapped = withStyles(({ color }) => ({
+        foo: {
+          color: color.red,
+        },
+      }))(MyComponent);
+      shallow(<Wrapped />);
+    });
+
+    it('uses the theme from context', () => {
+      const tropicalTheme = {
+        color: {
+          red: 'yellow',
+        },
+      };
+      ThemedStyleSheet.registerTheme('tropical', tropicalTheme);
+
+      function MyComponent({ styles }) {
+        expect(styles).to.eql({ foo: { color: 'yellow' } });
+        return null;
+      }
+
+      const Wrapped = withStyles(({ color }) => ({
+        foo: {
+          color: color.red,
+        },
+      }))(MyComponent);
+      shallow(<Wrapped />, { context: { themeName: 'tropical' } });
+    });
+
+    it('allows the styles prop name to be customized', () => {
+      function MyComponent({ bar }) {
+        expect(bar).to.eql({ foo: { color: '#ff0000' } });
+        return null;
+      }
+
+      const Wrapped = withStyles(() => ({}), { stylesPropName: 'bar' })(MyComponent);
+      shallow(<Wrapped />);
+    });
+
+    it('does not flush styles before rendering', () => {
+      function MyComponent() {
+        return null;
+      }
+
+      const Wrapped = withStyles(() => ({}))(MyComponent);
+      shallow(<Wrapped />);
+      expect(testInterface.flush.callCount).to.equal(0);
+    });
+
+    it('with the flushBefore option set, flushes styles before rendering', () => {
+      function MyComponent() {
+        return null;
+      }
+
+      const Wrapped = withStyles(() => ({}), { flushBefore: true })(MyComponent);
+      shallow(<Wrapped />);
+      expect(testInterface.flush.callCount).to.equal(1);
+    });
+
+    it('hoists statics', () => {
+      function MyComponent() {
+        return null;
+      }
+      MyComponent.foo = 'bar';
+
+      const Wrapped = withStyles(() => ({}), { flushBefore: true })(MyComponent);
+      expect(Wrapped.foo).to.equal('bar');
+    });
+
+    it('works with css()', () => {
+      function MyComponent({ styles }) {
+        return <div {...css(styles.foo)} />;
+      }
+      MyComponent.propTypes = {
+        styles: PropTypes.object.isRequired,
+      };
+
+      const Wrapped = withStyles(({ color }) => ({
+        foo: {
+          color: color.red,
+        },
+      }))(MyComponent);
+      const wrapper = shallow(<Wrapped />).first().shallow();
+
+      expect(wrapper.prop('style')).to.eql({ color: '#990000' });
+    });
+  });
+});
