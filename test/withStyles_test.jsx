a29bc5be7b3c78541ad678d234e2764aabff1f16
Jordan Harband <ljharb@gmail.com>
2017-11-26 12:26:33 -0800

    [Tests] use `safeSFC` from `enzyme-adapter-react-helper`

--- test/withStyles_test.jsx
+++ test/withStyles_test.jsx
@@ -6,6 +6,7 @@ import deepmerge from 'deepmerge';
 import sinon from 'sinon-sandbox';
 import DirectionProvider, { DIRECTIONS } from 'react-with-direction/dist/DirectionProvider';
 import ifReact from 'enzyme-adapter-react-helper/build/ifReact';
+import safeSFC from 'enzyme-adapter-react-helper/build/safeSFC';
 
 import ThemedStyleSheet from '../src/ThemedStyleSheet';
 import { css, cssNoRTL, withStyles, withStylesPropTypes } from '../src/withStyles';
@@ -60,7 +61,7 @@ describe('withStyles()', () => {
         return null;
       }
 
-      const WrappedComponent = withStyles(() => ({}))(MyComponent);
+      const WrappedComponent = withStyles(() => ({}))(safeSFC(MyComponent));
       shallow(<WrappedComponent />);
       expect(testInterface.create.callCount).to.equal(1);
     });
@@ -70,7 +71,7 @@ describe('withStyles()', () => {
         return null;
       }
 
-      const result = withStyles(() => ({}))(MyComponent);
+      const result = withStyles(() => ({}))(safeSFC(MyComponent));
       expect(result.displayName).to.equal('withStyles(MyComponent)');
     });
 
@@ -80,7 +81,7 @@ describe('withStyles()', () => {
         return null;
       }
 
-      const Wrapped = withStyles(() => ({}))(MyComponent);
+      const Wrapped = withStyles(() => ({}))(safeSFC(MyComponent));
       shallow(<Wrapped />).dive();
     });
 
@@ -90,7 +91,7 @@ describe('withStyles()', () => {
         return null;
       }
 
-      const Wrapped = withStyles(() => ({}), { themePropName: 'foo' })(MyComponent);
+      const Wrapped = withStyles(() => ({}), { themePropName: 'foo' })(safeSFC(MyComponent));
       shallow(<Wrapped />).dive();
     });
 
@@ -104,7 +105,7 @@ describe('withStyles()', () => {
         foo: {
           color: color.red,
         },
-      }))(MyComponent);
+      }))(safeSFC(MyComponent));
       shallow(<Wrapped />).dive();
     });
 
@@ -114,7 +115,7 @@ describe('withStyles()', () => {
         return null;
       }
 
-      const Wrapped = withStyles()(MyComponent);
+      const Wrapped = withStyles()(safeSFC(MyComponent));
       shallow(<Wrapped />).dive();
     });
 
@@ -128,7 +129,7 @@ describe('withStyles()', () => {
         foo: {
           color: '#ff0000',
         },
-      }), { stylesPropName: 'bar' })(MyComponent);
+      }), { stylesPropName: 'bar' })(safeSFC(MyComponent));
       shallow(<Wrapped />).dive();
     });
 
@@ -137,7 +138,7 @@ describe('withStyles()', () => {
         return null;
       }
 
-      const Wrapped = withStyles(() => ({}))(MyComponent);
+      const Wrapped = withStyles(() => ({}))(safeSFC(MyComponent));
       shallow(<Wrapped />);
       expect(testInterface.flush.callCount).to.equal(0);
     });
@@ -147,7 +148,7 @@ describe('withStyles()', () => {
         return null;
       }
 
-      const Wrapped = withStyles(() => ({}), { flushBefore: true })(MyComponent);
+      const Wrapped = withStyles(() => ({}), { flushBefore: true })(safeSFC(MyComponent));
       shallow(<Wrapped />);
       expect(testInterface.flush.callCount).to.equal(1);
     });
@@ -158,7 +159,7 @@ describe('withStyles()', () => {
       }
       MyComponent.foo = 'bar';
 
-      const Wrapped = withStyles(() => ({}), { flushBefore: true })(MyComponent);
+      const Wrapped = withStyles(() => ({}), { flushBefore: true })(safeSFC(MyComponent));
       expect(Wrapped.foo).to.equal('bar');
     });
 
@@ -174,7 +175,7 @@ describe('withStyles()', () => {
         foo: {
           color: color.red,
         },
-      }))(MyComponent);
+      }))(safeSFC(MyComponent));
       const wrapper = shallow(<Wrapped />).dive();
 
       expect(wrapper.prop('style')).to.eql({ color: '#990000' });
@@ -198,7 +199,7 @@ describe('withStyles()', () => {
         foo: {
           color: color.red,
         },
-      }))(MyComponent);
+      }))(safeSFC(MyComponent));
 
       // copied
       const expectedPropTypes = deepmerge({}, MyComponent.propTypes);
@@ -219,7 +220,7 @@ describe('withStyles()', () => {
         return null;
       }
 
-      const Wrapped = withStyles(() => ({}))(MyComponent);
+      const Wrapped = withStyles(() => ({}))(safeSFC(MyComponent));
       const wrappedProto = Object.getPrototypeOf(Wrapped);
       const wrappedProtoProto = Object.getPrototypeOf(Wrapped.prototype);
       expect(wrappedProto).to.equal(React.Component);
@@ -235,7 +236,7 @@ describe('withStyles()', () => {
         return null;
       }
 
-      const Wrapped = withStyles(() => ({}), { pureComponent: true })(MyComponent);
+      const Wrapped = withStyles(() => ({}), { pureComponent: true })(safeSFC(MyComponent));
       expect(Object.getPrototypeOf(Wrapped)).not.to.equal(React.Component);
       expect(Object.getPrototypeOf(Wrapped.prototype)).not.to.equal(React.Component.prototype);
       expect(Object.getPrototypeOf(Wrapped)).to.equal(React.PureComponent);
@@ -245,18 +246,18 @@ describe('withStyles()', () => {
 
   describe('css/cssNoRTL', () => {
     it('css calls resolve method', () => {
-      function MyComponent() {
+      const MyComponent = safeSFC(function MyComponent() {
         return <div {...css({ color: 'red' })} />;
-      }
+      });
 
       shallow(<MyComponent />);
       expect(testInterfaceResolveStub.callCount).to.equal(1);
     });
 
     it('cssNoRTL calls resolve method if resolveNoRTL does not exist', () => {
-      function MyComponent() {
+      const MyComponent = safeSFC(function MyComponent() {
         return <div {...cssNoRTL({ color: 'red' })} />;
-      }
+      });
 
       shallow(<MyComponent />);
       expect(testInterfaceResolveStub.callCount).to.equal(1);
@@ -296,9 +297,9 @@ describe('RTL support', () => {
 
   describe('css/cssNoRTL', () => {
     it('css calls resolve method', () => {
-      function MyComponent() {
+      const MyComponent = safeSFC(function MyComponent() {
         return <div {...css({ color: 'red' })} />;
-      }
+      });
 
       shallow(<MyComponent />);
       expect(resolveStub.callCount).to.equal(1);
@@ -306,9 +307,9 @@ describe('RTL support', () => {
     });
 
     it('cssNoRTL calls resolve method if resolveNoRTL does not exist', () => {
-      function MyComponent() {
+      const MyComponent = safeSFC(function MyComponent() {
         return <div {...cssNoRTL({ color: 'red' })} />;
-      }
+      });
 
       shallow(<MyComponent />);
       expect(resolveStub.callCount).to.equal(0);
@@ -322,7 +323,7 @@ describe('RTL support', () => {
         return null;
       }
 
-      const WrappedComponent = withStyles(() => ({}))(MyComponent);
+      const WrappedComponent = withStyles(() => ({}))(safeSFC(MyComponent));
       render(<WrappedComponent />);
       expect(testInterface.create).to.have.property('callCount', 1);
     });
@@ -332,7 +333,7 @@ describe('RTL support', () => {
         return null;
       }
 
-      const WrappedComponent = withStyles(() => ({}))(MyComponent);
+      const WrappedComponent = withStyles(() => ({}))(safeSFC(MyComponent));
       render((
         <DirectionProvider direction={DIRECTIONS.LTR}>
           <WrappedComponent />
@@ -346,7 +347,7 @@ describe('RTL support', () => {
         return null;
       }
 
-      const WrappedComponent = withStyles(() => ({}))(MyComponent);
+      const WrappedComponent = withStyles(() => ({}))(safeSFC(MyComponent));
       render((
         <DirectionProvider direction={DIRECTIONS.RTL}>
           <WrappedComponent />
